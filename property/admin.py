377d1e0c7d1838fc785180e4e3b1bc7d1c5c8bbb
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-03 16:42:24 +0500

    Add inline owners view on Flat admin page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnersInline(admin.StackedInline):
+    model = Flat.owner_set.through
+    raw_id_fields = ('owner',)
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'town_district', 'address')
     readonly_fields = ['created_at']
@@ -11,6 +16,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
     raw_id_fields = ('like',)
+    inlines = [OwnersInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
